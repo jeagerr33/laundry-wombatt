f0c68ad5e32f3a7cf12f52654e41bbb385869ac6
andrea devi <141018391+6182101033@users.noreply.github.com>
2024-12-02 22:06:24 +0700

    Add files via upload

---
+++ Pelanggann/PelangganView.java
@@ -0,0 +1,14 @@
+package com.example.demo.PelangganView;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PelangganView {
+    private int id;        
+    private String nama;    
+    private String noHP;    
+    private String email; 
+}
